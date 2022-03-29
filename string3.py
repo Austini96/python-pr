8508324fa0916dab07152743e5e408dcbd978aef
Austin Lee <101227658+Austini96@users.noreply.github.com>
2022-03-28 19:02:33 -0500

    Add files via upload
    Grammar : putting in variables
    
    name -> '+name+'
    age -> '+age+'

---
+++ string3.py
@@ -0,0 +1,3 @@
+name='google'
+age=12
+print('to '+name+'. Lorem ipsum dolor sit amet, consectetur apple computer adipisicing elit, sed do eiusmod tempor apple incididunt ut labore et dolore magna aliqua. '+name+' Ut enim ad minim veniam, quis nostrud exercitation ullamco '+age+' laboris nisi ut aliquip ex ea commodo consequat. '+name+' Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
