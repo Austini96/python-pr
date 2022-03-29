d63d4b59f9bf18e38bc047ee691d58780989269c
Austin Lee <101227658+Austini96@users.noreply.github.com>
2022-03-28 19:06:01 -0500

    Add files via upload
    Grammar : formatting
    Positional formatting - {}, {}    put ".format('a', #(number), 'b', 'c')" at the end
    Named placeholder - {name}, {age}, {age:d}    put ".format(name='a', age=12" at the end
                                       for "age:d", only digits are allowed

---
+++ string4.py
@@ -0,0 +1,5 @@
+#positional formatting
+print('to {}. Lorem ipsum dolor sit amet, consectetur computer adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. {} Ut enim ad minim veniam, quis nostrud exercitation ullamco {} laboris nisi ut aliquip ex ea commodo consequat. {} Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'.format('google', 12, 'google', 'google'))
+
+#Named placeholder
+print('to {name}. Lorem ipsum dolor sit amet, consectetur computer adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. {age:d} Ut enim ad minim veniam, quis nostrud exercitation ullamco {name} laboris nisi ut aliquip ex ea commodo consequat. {name} Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'.format(name='google', age=12))
